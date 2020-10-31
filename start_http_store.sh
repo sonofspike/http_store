b3de4118f20ed31f5707bbfbf62a60634f65741a
Hanen Garcia <hanengarcia@users.noreply.github.com>
2020-10-31 13:38:01 -0400

    Fixed port bindings error

--- start_http_store.sh
+++ start_http_store.sh
@@ -14,7 +14,6 @@ podman pod create --name http-store -p $HTTP_PORT
 podman run -dt --pod http-store --env-file environment \
     -v $PWD/nginx.conf:/bitnami/nginx/conf/nginx.conf \
     -v $HTTP_STORE_PATH:/var/www/files \
-    -p $HTTP_PORT:8888 \
     --name store quay.io/bitnami/nginx:latest
 
 echo # Access HTTP Store at http://localhost:8080
